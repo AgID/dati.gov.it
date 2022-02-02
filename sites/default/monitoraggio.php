286079d977871f26c226b00d6724a79e1a304936
l.ebraico <l.ebraico@almaviva.it>
2022-02-02 08:30:00 +0100

    initial commit

--- sites/default/monitoraggio.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: https://datawrapper.dwcdn.net');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
-$file=file_get_contents("https://docs.google.com/spreadsheets/d/e/2PACX-1vTfexl7t2kg_yGXgoPrTD2ab92QNhuKDGOAgGSE9XPn7jX9ITyyzLfIAEb9inZTudWyzLoKpprmWOx5/pub?gid=384894090&single=true&output=csv");
-echo $file;
-
- ?>
--- sites/default/txt/monitoraggioict.php
+++
@@ -1,8 +0,0 @@
-<?php   
-header('Access-Control-Allow-Origin: https://datawrapper.dwcdn.net');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
-$file=file_get_contents('/var/www/html/sites/default/txt/monitoraggioict.csv');
-
-echo $file
-
-?>
